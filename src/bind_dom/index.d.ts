f4a06d1dd0b2e261bd648b0c09bf12574dedeebe
Brian Takita <brian.takita@gmail.com>
2023-03-15 02:19:25 -0400

    bind_dom__validate: error: fix: printing bind_dom_name
    variable names: bind_dom_id=>bind_dom_name

--- src/bind_dom/index.d.ts
+++ src/bind_dom/index.d.ts
@@ -3,8 +3,8 @@ export declare function bind_dom<
 	E extends Element = Element
 >(ctx:Ctx, parent?:E):void
 export declare function bind_dom__register(ctx:Ctx, bind_dom:bind_dom_T):void
-export declare function bind_dom__register(ctx:Ctx, bind_dom_id:string, bind_dom:bind_dom_T):void
-export declare function bind_dom__validate(ctx:Ctx, bind_dom_id?:string):void
+export declare function bind_dom__register(ctx:Ctx, bind_dom_name:string, bind_dom:bind_dom_T):void
+export declare function bind_dom__validate(ctx:Ctx, bind_dom_name?:string):void
 export type bind_dom_T<
 	E extends Element = Element
 > = ((el:E, ctx:Ctx)=>void)|((el:E)=>void)
--- src/bind_dom/index.js
+++ src/bind_dom/index.js
@@ -19,9 +19,9 @@ export function bind_dom(ctx, parent = document) {
 	const bind_dom_name_M_bind_dom = bind_dom_name_M_bind_dom_(ctx)
 	const bind_dom_M_call_count = bind_dom_M_call_count_(ctx)
 	for (const el of parent__el_a) {
-		const bind_dom_id = el.dataset.bind_dom
-		const _bind_dom = bind_dom_name_M_bind_dom.get(bind_dom_id)
-		if (!_bind_dom) bind_dom__register__validate__throw(bind_dom_id)
+		const bind_dom_name = el.dataset.bind_dom
+		const _bind_dom = bind_dom_name_M_bind_dom.get(bind_dom_name)
+		if (!_bind_dom) bind_dom__register__validate__throw(bind_dom_name)
 		bind_dom_M_call_count.set(
 			_bind_dom,
 			bind_dom_M_call_count.get(_bind_dom) + 1)
@@ -30,60 +30,63 @@ export function bind_dom(ctx, parent = document) {
 }
 /**
  * @param {Ctx}ctx
- * @param {bind_dom_T|string}bind_dom_id
+ * @param {bind_dom_T|string}bind_dom_name
  * @param {bind_dom_T}[bind_dom]
  */
 export function bind_dom__register(
 	ctx,
-	bind_dom_or_bind_dom_id,
+	bind_dom_or_bind_dom_name,
 	bind_dom
 ) {
-	const bind_dom_id =
-		typeof bind_dom_or_bind_dom_id === 'string'
-		? bind_dom_or_bind_dom_id
-		: bind_dom_or_bind_dom_id.name
-	if (!bind_dom_id) {
-		throw new Error(`bind_dom_id is required: bind_dom_id: ${JSON.stringify(bind_dom_id)}`)
+	const bind_dom_name =
+		typeof bind_dom_or_bind_dom_name === 'string'
+		? bind_dom_or_bind_dom_name
+		: bind_dom_or_bind_dom_name.name
+	if (!bind_dom_name) {
+		throw new Error(`bind_dom_name is required: bind_dom_name: ${JSON.stringify(bind_dom_name)}`)
 	}
-	if (typeof bind_dom_or_bind_dom_id === 'function') {
-		bind_dom = bind_dom_or_bind_dom_id
+	if (typeof bind_dom_or_bind_dom_name === 'function') {
+		bind_dom = bind_dom_or_bind_dom_name
 	}
-	bind_dom_name_M_bind_dom_(ctx).set(bind_dom_id, bind_dom)
+	bind_dom_name_M_bind_dom_(ctx).set(bind_dom_name, bind_dom)
 	bind_dom_M_call_count_(ctx).set(bind_dom, 0)
 }
 /**
  * @param {Ctx}ctx
- * @param {string}[bind_dom_id]
+ * @param {string}[bind_dom_name]
  */
-export function bind_dom__validate(ctx, bind_dom_id) {
+export function bind_dom__validate(ctx, bind_dom_name) {
 	const bind_dom_name_M_bind_dom = bind_dom_name_M_bind_dom_(ctx)
-	if (bind_dom_id) {
-		if (!bind_dom_name_M_bind_dom.get(bind_dom_id)) {
-			bind_dom__register__validate__throw(bind_dom_id)
+	if (bind_dom_name) {
+		if (!bind_dom_name_M_bind_dom.get(bind_dom_name)) {
+			bind_dom__register__validate__throw(bind_dom_name)
 		}
 	} else {
 		const bind_dom_M_call_count = bind_dom_M_call_count_(ctx)
-		for (const bind_dom of bind_dom_name_M_bind_dom.values()) {
+		for (const [
+			bind_dom_name,
+			bind_dom
+		] of bind_dom_name_M_bind_dom.entries()) {
 			if (!bind_dom_M_call_count.get(bind_dom)) {
-				bind_dom__call_count__validate__throw(bind_dom_id)
+				bind_dom__call_count__validate__throw(bind_dom_name)
 			}
 		}
 	}
 }
 /**
- * @param {string}bind_dom_id
+ * @param {string}bind_dom_name
  */
-function bind_dom__register__validate__throw(bind_dom_id) {
-	throw new Error(`bind_dom__register: bind_dom_id ${
-		JSON.stringify(bind_dom_id)} not registered: ${''
+function bind_dom__register__validate__throw(bind_dom_name) {
+	throw new Error(`bind_dom__register: bind_dom_name ${
+		JSON.stringify(bind_dom_name)} not registered: ${''
 	}use bind_dom__register or make sure the data-bind_dom html attribute is correct`)
 }
 /**
- * @param {string}bind_dom_id
+ * @param {string}bind_dom_name
  */
-function bind_dom__call_count__validate__throw(bind_dom_id) {
-	const bind_dom_id__json = JSON.stringify(bind_dom_id)
-	throw new Error(`bind_dom__call_count: bind_dom_id ${
-		bind_dom_id__json} registered & not called: ${''
-	}please remove the call to bind_dom__register(ctx, ${bind_dom_id__json}, bind_dom)`)
+function bind_dom__call_count__validate__throw(bind_dom_name) {
+	const bind_dom_name__json = JSON.stringify(bind_dom_name)
+	throw new Error(`bind_dom__call_count: bind_dom_name ${
+		bind_dom_name__json} registered & not called: ${''
+	}please remove the call to bind_dom__register(ctx, ${bind_dom_name__json}, bind_dom)`)
 }
