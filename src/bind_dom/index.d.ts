051f6bc1cdba620c77651e0c50a18f5f0b7f9586
Brian Takita <brian.takita@gmail.com>
2023-03-16 03:14:08 -0400

    + arg1__bind_dom_T,arg2__bind_dom_T
    bind_dom_T<E extends Element = Element> = arg1__bind_dom_T<E>|arg2__bind_dom_T<E>
    
    + arg1__id__bind_dom_T,arg2__id__bind_dom_T
    
    id__bind_dom_T<E extends Element = Element> = arg1__id__bind_dom_T<E>|arg2__id__bind_dom_T<E>

--- src/bind_dom/index.d.ts
+++ src/bind_dom/index.d.ts
@@ -4,14 +4,31 @@ export declare function bind_dom<
 >(ctx:Ctx, parent?:E):void
 export declare function id__bind_dom_<
 	E extends Element = Element
->(id:string, bind_dom:bind_dom_T):id__bind_dom_T
+>(id:string, bind_dom:arg1__bind_dom_T<E>):arg1__id__bind_dom_T<E>
+export declare function id__bind_dom_<
+	E extends Element = Element
+>(id:string, bind_dom:arg2__bind_dom_T<E>):arg2__id__bind_dom_T<E>
 export {
 	id__bind_dom_ as bind_dom_,
 }
 export declare function bind_dom__register(ctx:Ctx, bind_dom:bind_dom_T):void
 export declare function bind_dom__register(ctx:Ctx, bind_dom_name:string, bind_dom:bind_dom_T):void
 export declare function bind_dom__validate(ctx:Ctx, bind_dom_name?:string):void
+export type arg1__bind_dom_T<
+	E extends Element = Element
+> = (el:E)=>void
+export type arg2__bind_dom_T<
+	E extends Element = Element
+> = (el:E, ctx:Ctx)=>void
 export type bind_dom_T<
 	E extends Element = Element
-> = ((el:E, ctx?:Ctx)=>void)|((el:E)=>void)
-export type id__bind_dom_T = bind_dom_T&{ id:string }
+> = arg1__bind_dom_T<E>|arg2__bind_dom_T<E>
+export type arg1__id__bind_dom_T<
+	E extends Element = Element
+> = ((el:E)=>void)&{ id:string }
+export type arg2__id__bind_dom_T<
+	E extends Element = Element
+> = ((el:E, ctx:Ctx)=>void)&{ id:string }
+export type id__bind_dom_T<
+	E extends Element = Element
+> = arg1__id__bind_dom_T<E>|arg2__id__bind_dom_T<E>
