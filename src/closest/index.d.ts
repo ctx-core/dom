346b699a7a690dee2810be45995bd080a91e2843
Brian Takita <brian.takita@gmail.com>
2023-03-24 03:11:13 -0400

    closest: + generic return type

--- src/closest/index.d.ts
+++ src/closest/index.d.ts
@@ -3,8 +3,10 @@ import type { selector_T } from '../_types'
  * Returns the first matching dom element in el -> ...parent
  * @returns {*|Node}
  */
-export declare function closest(
+export declare function closest<
+	E extends ParentNode = ParentNode
+>(
 	selector:selector_T,
 	element:HTMLElement,
 	self_check?:boolean
-):ParentNode|undefined
+):E|undefined
